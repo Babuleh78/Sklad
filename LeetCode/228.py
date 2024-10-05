ddaee7124f5bbabd13f6ab1830ceeae38ed14948
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-10-05 22:47:22 +0300

    shah

---
+++ LeetCode/228.py
@@ -0,0 +1,28 @@
+class Solution:
+    def summaryRanges(self, nums: list[int]) -> list[str]:
+        if not nums:
+            return []
+
+        s = []
+        first = nums[0]
+        second = first
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1] + 1:
+                second = nums[i]
+            else:
+                if first == second:
+                    s.append(str(first))
+                else:
+                    s.append(f"{first}->{second}")
+                first = nums[i]
+                second = first
+        if first == second:
+            s.append(str(first))
+        else:
+            s.append(f"{first}->{second}")
+
+        return s
+
+s = Solution().summaryRanges([0, 2, 3, 4, 6, 8, 9])
+print(s)  
---
+++ LeetCode/FirstPython.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+        return []  # No solution found
+a = Solution.twoSum(Solution(),[2, 7, 12, 15], 9)
+print(a)
