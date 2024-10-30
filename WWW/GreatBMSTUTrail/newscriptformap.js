e87378ac0c611654b97d4384e27bd7d201879196
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-10-30 18:48:54 +0300

    Шашура

--- WWW/GreatBMSTUTrail/newscriptformap.js
+++ WWW/GreatBMSTUTrail/newscriptformap.js
@@ -1,11 +1,51 @@
 const key = 'F3Eh4Wsa3QzZb0pMVkSZ';
 const map = L.map('map').setView([55.75476845750829,37.621749677246086], 11); //starting position
+let visitors = 0;
+class Hamster{
+  constructor(is_tap, all_count){
+    this.is_tap = is_tap;
+    this.all_count = all_count;
+  }
+}
+class information{
+  constructor(info_, number_){
+      this.info = info_;
+      this.number = number_;
+  }
+}
+const myballoonTemplate = `
+    <div class="balloon" id="balloon">
+        <div class="balloon_info"> 
+          <span id="info">текст</span>
+        </div>
+        <div class="balloon_num">Число посетителей
+          <span id="visitorCount">0</span>
+        </div>
+        <div class="difficult">
+            <div class="star-box">
+                Сложность
+                <i class="fas fa-star checked"></i>
+                <i class="fas fa-star checked"></i>
+                <i class="fas fa-star checked"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+        </div>
+        <button id="ZButton" class="ZButton">
+            <div class="ZButton_text">ЛИКВИДИРОВАТЬ</div>
+        </button>
+    </div>
+`;
 
+// Создаем массив с уникальными balloon-ами
+let balloons = [myballoonTemplate, myballoonTemplate, myballoonTemplate];
+let coor = [[55.7558, 37.6173], [55.8033638275589,37.409635388619094], [55.754137466243,37.64899523354594]];
+let count = coor.length; 
 let info = ["Раковая опухоль на теле Строгино. Таллинская 34. Прознав о нахождении в Строгино штаб-квартиры агента, ВШивые студенты возвели напротив \
     это убожество архитектуры под названием МИЭМ",
-    "Да-да, она существует, военная кафедра вшэ, что сказать, служу России"
+    "Да-да, она существует, военная кафедра вшэ, что сказать, служу России",
+    "еще какая-нибудь хуйня"
 ];    
-
 L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
   tileSize: 512,
   zoomOffset: -1,
@@ -13,30 +53,53 @@ L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}
   attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
   crossOrigin: true
 }).addTo(map);
-const testdrive =`
-    <div class="balloon">
-        <div class="balloon_info"> ${info[0]}</div>
-        <div class="balloon_num">Число посетителей
-          <span id="visitorCount">0</span>
+
+for (let i = 0; i < count; i++) {
+  // Создаем уникальные идентификаторы для каждого маркера
+  const uniqueId = `popup-${i}`;
+  const uniqueZButtonId = `ZButton-${i}`;
+  const uniqueVisitorCountId = `visitorCount-${i}`;
+  const uniqueInfoId = `info-${i}`;
+
+  const balloonHTML = `
+    <div class="balloon" id="${uniqueId}">
+        <div class="balloon_info"> 
+          <span id="${uniqueInfoId}">текст</span>
         </div>
-            <div class = "difficult">
-              <div class = "star-box">
+        <div class="balloon_num">Число посетителей
+          <span id="${uniqueVisitorCountId}">0</span>
+        </div>
+        <div class="difficult">
+            <div class="star-box">
                 Сложность
                 <i class="fas fa-star checked"></i>
                 <i class="fas fa-star checked"></i>
                 <i class="fas fa-star checked"></i>
                 <i class="fas fa-star"></i>
                 <i class="fas fa-star"></i>
-              </div>
             </div>
-            <button id = "ZButton" class="ZButton">
-              <div class = "ZButton_text">ЛИКВИДИРОВАТЬ</div>
-            </button>
         </div>
+        <button id="${uniqueZButtonId}" class="ZButton">
+            <div class="ZButton_text">ЛИКВИДИРОВАТЬ</div>
+        </button>
+    </div>
+`;
 
-`
-const marker = L.marker([55.7558, 37.6173]) // Широта и долгота маркера
-.addTo(map)
-.bindPopup(testdrive); // Добавление всплывающего окна
+  const marker = L.marker(coor[i])
+      .addTo(map)
+      .bindPopup(balloonHTML); // Добавление всплывающего окна
 
-    
+  marker.on('popupopen', function() {
+      const ZButton = document.getElementById(uniqueZButtonId);
+      const visitorCount = document.getElementById(uniqueVisitorCountId);
+      const infospan = document.getElementById(uniqueInfoId);
+
+      // Устанавливаем уникальный текст для каждого всплывающего окна
+      infospan.textContent = info[i]; // Убедитесь, что info[i] существует
+
+      ZButton.addEventListener("click", function() {
+          visitors++;
+          visitorCount.textContent = visitors;
+      });
+  });
+}
