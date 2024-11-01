bd8e3fc1d264b58dfc8dbb39f74a2cbf0e17dc94
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-11-01 18:40:00 +0300

    CОСАТЬ ВЫШКА СОСАТЬ ВЫШКА СОСАТЬ ВЫШКА

--- WWW/GreatBMSTUTrial/captcha.js
+++ WWW/GreatBMSTUTrial/captcha.js
@@ -1,16 +1,23 @@
 const buttons = document.querySelectorAll('.image-button');
-console.log("Скрипт Капча");
+const container = document.getElementById("container");
+var taps = 0;
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         console.log("Нажатие обработано");
         if (button.classList.contains('active')) {
             button.classList.remove('active');
+            taps-=1;
             console.log("Убрали");
         } else {
             button.classList.add('active'); 
             console.log("Добавили");
+            taps+=1;
         }
     });
 });
-
-console.log("Сработал");
+const go_button = document.getElementById("go-button");
+go_button.addEventListener('click', ()=>{
+    if(taps == 16){
+        container.style.visibility = 'hidden';
+    }
+});
