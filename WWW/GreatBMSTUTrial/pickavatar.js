94f30ea169e672c75a371ac7b0af6e4830af682c
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-11-12 17:11:47 +0300

    Not one of you will be left alive

---
+++ WWW/GreatBMSTUTrial/pickavatar.js
@@ -0,0 +1,33 @@
+const avatar = document.getElementById("avatar");
+const av1 = document.getElementById("avatarpick1");
+const av2 = document.getElementById("avatarpick2");
+const av3 = document.getElementById("avatarpick3");
+const av4 = document.getElementById("avatarpick4");
+const av5 = document.getElementById("avatarpick5");
+const av6 = document.getElementById("avatarpick6");
+const av7 = document.getElementById("avatarpick7");
+const av8 = document.getElementById("avatarpick8");
+av1.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/BAZA.jpeg')";
+}
+av2.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/BEGEMOT85.jpg')";
+}
+av3.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/FUCK.png')";
+}
+av4.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/ZVEZDA.png')";
+}
+av5.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/PIVO.png')";
+}
+av6.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/SLON.png')";
+}
+av7.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/OCHKI.jpg')";
+}
+av8.onclick = function(){
+    avatar.style.backgroundImage = "url('avatars/KPTCH.jpg')";
+}
