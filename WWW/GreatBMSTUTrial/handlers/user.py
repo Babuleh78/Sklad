4d3929128c1fea48f71cf5e040015851de94b2ce
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-11-22 21:51:02 +0300

    ЗДЕСЬ ЖЕ ЧЕРНЫМ ПО БЕЛОМУ НАПИСАНО

--- WWW/GreatBMSTUTrial/handlers/user.py
+++ WWW/GreatBMSTUTrial/handlers/user.py
@@ -9,8 +9,9 @@ from aiogram.types import InputFile
 from aiogram.filters import CommandStart, Command, or_f
 from klava import reply, inline
 from klava.reply import get_keyboard
+from klava.inline import get_callback_btns
 user_router = Router()
-ADMIN_ID = os.getenv('ADMIN')
+ADMIN_ID = int(os.getenv('ADMIN'))
 IS_ADMIN = False
 IGNORE_CASE = False
 bot = Bot(token = os.getenv('TOKEN'))
@@ -81,12 +82,31 @@ async def get_image():
             
             if not result:
                 return ["Изображение не найдено!"]
-            id, name, url = result[0]
-            data = [name, url]
+            id, name, url, uid, pid = result[0]
+            data = [name, url, pid]
             return data
     except Exception as e:
         print("Ошибка выполнения SQL-запроса:", e)
         return []  
+async def good_photo():
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT* FROM images ORDER BY image_id limit 1"
+            cursor.execute(query)
+            result = cursor.fetchone() 
+            if not result:
+                return
+            id, name, url, uid, pid = result
+            query1 = "UPDATE visits SET is_visit = %s WHERE user_id = %s AND place_id = %s"
+            cursor.execute(query1, (1, uid, pid))  
+            connection.commit()  
+            return
+    except Exception as e:
+        print("Ошибка выполнения SQL-запроса:", e)
+        return
+
+
+
 @user_router.message(or_f(Command('top'), (F.text.lower().contains("топ"))))
 async def process_callback_button(message: types.Message):
     await message.answer("Топ 5 лучших участников")
@@ -120,18 +140,42 @@ def base64_to_image(base64_str):
 
 @user_router.message(F.text.lower().contains("а"))
 async def info_panel(message: types.Message):
-    await message.answer("Будем считать, что ты админ")
-    name, base64_str = await get_image()
-    header, base64_data = base64_str.split(',', 1)
-    decode = base64.b64decode(base64_data)
-    img = Image.open(io.BytesIO(decode))
-    img.show()
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as tmp_file:
-        img.save(tmp_file, format='PNG')
-        tmp_file.seek(0) 
-        photo = types.FSInputFile(tmp_file.name)
-    # Отправляем изображение
-    await message.answer_photo(photo=photo)
+    print(message.from_user.id)
+    if message.from_user.id == ADMIN_ID:
+        await message.answer("Будем считать, что ты админ")
+        
+        name, base64_str, pid = await get_image()
+        str_message = f'Пользователь {name} прислал вам изображение, он хочет ликвидировать место {pid}. Одобряем?'
+        
+        header, base64_data = base64_str.split(',', 1)
+        decode = base64.b64decode(base64_data)
+        img = Image.open(io.BytesIO(decode))
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as tmp_file:
+            img.save(tmp_file, format='PNG')
+            tmp_file.seek(0) 
+            photo = types.FSInputFile(tmp_file.name)
+
+        buttons = {
+            "ДА": "yes_callback", 
+            "НЕТ": "no_callback",   
+        }
+        keyboard = get_callback_btns(btns=buttons)
+        await message.answer_photo(photo=photo, caption=str_message, reply_markup=keyboard)
+
+        os.remove(tmp_file.name)
+    else:
+        await message.answer("Соси хуй")
+
+@user_router.callback_query(lambda c: c.data in ["yes_callback", "no_callback"])
+async def process_callback(callback_query: types.CallbackQuery):
+    if callback_query.data == "yes_callback":
+        await bot.answer_callback_query(callback_query.id) 
+        await good_photo()
+        await bot.send_message(callback_query.from_user.id, "Ликвидация успешна")
+    elif callback_query.data == "no_callback":
+        await bot.answer_callback_query(callback_query.id)  # Подтверждаем нажатие
+        await bot.send_message(callback_query.from_user.id, "Вы выбрали: НЕТ")
 
 @user_router.message(F.text.lower().contains("ш"))
 async def info_panel(message: types.Message):
