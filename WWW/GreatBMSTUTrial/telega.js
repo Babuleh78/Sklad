a04e10869e9e124e60236815780b4b4d2ee50fe2
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-11-23 18:39:54 +0300

    Привет Никита Шашушаш!

---
+++ WWW/GreatBMSTUTrial/telega.js
@@ -0,0 +1,18 @@
+const telega = document.getElementById("telega");
+const closeModalButton = document.getElementById("closeTelega");
+const go_btn = document.getElementById("go_to_telega");
+go_btn.addEventListener("click", function(){
+    window.open("https://t.me/ForGreatBMSTUTrailBot");
+});
+
+
+closeModalButton.addEventListener("click", function() {
+    telega.style.display = "none";
+});
+
+// Закрываем модальное окно при клике вне его
+window.addEventListener("click", function(event) {
+    if (event.target === telega) {
+        telega.style.display = "none";
+    }
+});
