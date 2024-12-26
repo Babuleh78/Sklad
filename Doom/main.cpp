29506ac7402ee5643010af573ba8aa954fbaff8c
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2024-12-26 22:03:00 +0300

    Update main.cpp

--- Doom/main.cpp
+++ Doom/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <string>
+#include <chrono>
 int ScreenWidth = 120;
 int ScreenHeight = 40;
 
@@ -37,7 +38,43 @@ int main()
     map += L"#..............#";
     map += L"#..............#";
     map += L"################";
+
+    auto tp1 = std::chrono::system_clock::now();
+    auto tp2 = std::chrono::system_clock::now();
     while (1) {
+        tp2 = std::chrono::system_clock::now();
+        std::chrono::duration<float> elTime = tp2 - tp1;
+        tp1 = tp2;
+        float felTime = elTime.count();
+
+
+
+
+        if (GetAsyncKeyState((unsigned short)'A') & 0x8000) {
+            PlayerA -= (0.7f) * felTime;
+        }
+
+        if (GetAsyncKeyState((unsigned short)'D') & 0x8000) {
+            PlayerA += (0.7f) * felTime;
+        }
+
+        if (GetAsyncKeyState((unsigned short)'W') % 0x8000) {
+            PlayerX += sinf(PlayerA) * 5.0f * felTime;
+            PlayerY += cosf(PlayerA) * 5.0f * felTime;
+        }
+
+        if (GetAsyncKeyState((unsigned short)'S') % 0x8000) {
+            PlayerX -= sinf(PlayerA) * 5.0f * felTime;
+            PlayerY -= cosf(PlayerA) * 5.0f * felTime;
+        }
+
+
+
+
+
+
+
+
         for (int x = 0; x < ScreenWidth; x++) {
             float Angle = (PlayerA - Pov / 2.0f) + ((float)x / (float)ScreenWidth) * Pov;
 
@@ -65,16 +102,49 @@ int main()
             int Ceiling = (float)(ScreenHeight / 2.0) - ScreenHeight / ((float)DistanceToWall);
             int Floor = ScreenHeight - Ceiling;
 
+            short Shade = ' ';
+            if (DistanceToWall <= Depth / 4.0f) {
+                Shade = 0x2588;
+            }
+            else if (DistanceToWall < Depth / 3.0f) {
+                Shade = 0x2593;
+            }
+            else if (DistanceToWall < Depth / 2.0f) {
+                Shade = 0X2592;
+            }
+            else if (DistanceToWall < Depth) {
+                Shade = 0x2591;
+            }
+            else {
+                Shade = ' ';
+            }
 
             for (int y = 0; y < ScreenHeight; y++) {
                 if (y < Ceiling) {
                     screen[y * ScreenWidth + x] = ' ';
                 }
                 else if(y > Ceiling && y <= Floor) {
-                    screen[y * ScreenWidth + x] = '#';
+                    screen[y * ScreenWidth + x] = Shade;
                 }
-                else {
-                    screen[y * ScreenWidth + x] = ' ';
+                else {//Пол
+                    float hui = 1.0f - (((float)y - ScreenHeight / 2.0f) / ((float)ScreenHeight / 2.0f));
+                    if (hui < 0.25) {
+                        Shade = '#';
+                    }
+                    else if (hui < 0.5) {
+                        Shade = 'x';
+                    }
+                    else if (hui < 0.75) {
+                        Shade = '.';
+                    }
+                    else if (hui < 1) {
+                        Shade = '-';
+                    }
+                    else  {
+                        Shade = ' ';
+                    }
+                    screen[y * ScreenWidth + x] = Shade;
+                    //screen[y * ScreenWidth + x] = ' ';
                 }
             }
 
