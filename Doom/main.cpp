6a48f21c9609e5a8255c718f92406c425ca56eef
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2024-12-26 19:04:21 +0300

    Update main.cpp

--- Doom/main.cpp
+++ Doom/main.cpp
@@ -1,8 +1,22 @@
 
 #include <iostream>
+#include <Windows.h>
 
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 0.0f;
+float fPlayerY = 0.0f;
+float fPlayer = 0.0f;
 int main()
 {
-    std::cout << "Hello World!\n";
+    wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+    while (1) {
+        screen[nScreenWidth * nScreenHeight - 1] = '\0';
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+    }
 }
 
