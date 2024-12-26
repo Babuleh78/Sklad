71809a3df92a133b9e333da06ee6f9af82ae2d70
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2024-12-26 20:29:52 +0300

    Update main.cpp

--- Doom/main.cpp
+++ Doom/main.cpp
@@ -5,14 +5,15 @@
 int ScreenWidth = 120;
 int ScreenHeight = 40;
 
-float PlayerX = 0.0f;
-float PlayerY = 0.0f;
+float PlayerX = 8.0f;
+float PlayerY = 8.0f;
 float PlayerA = 0.0f;
 
-int MapHeight = 15;
-int MapWidth = 15;
+int MapHeight = 16;
+int MapWidth = 16;
 
 float Pov = 3.14 / 4.0;
+float Depth = 16.0f;
 int main()
 {
     wchar_t* screen = new wchar_t[ScreenWidth * ScreenHeight];
@@ -21,21 +22,21 @@ int main()
     DWORD dwBytesWritten = 0;
 
     std::wstring map;
-    map += L"###############";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"#.............#";
-    map += L"###############";
+    map += L"################";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"################";
     while (1) {
         for (int x = 0; x < ScreenWidth; x++) {
             float Angle = (PlayerA - Pov / 2.0f) + ((float)x / (float)ScreenWidth) * Pov;
@@ -46,11 +47,35 @@ int main()
             float EyeX = sinf(Angle);
             float EyeY = cosf(Angle);
 
-            while (!HitWall) {
+            while (!HitWall && DistanceToWall < Depth) {
                 DistanceToWall += 0.1f;
                 int FakeX = (int)(PlayerX + EyeX * DistanceToWall);
                 int FakeY = (int)(PlayerY + EyeY * DistanceToWall);
                 
+                if (FakeX < 0 || FakeX >= MapWidth || FakeY < 0 || FakeY >= MapWidth) {
+                    HitWall = true;
+                    DistanceToWall = Depth;
+                }
+                else {
+                    if (map[FakeY * MapWidth + FakeX] == '#') {
+                        HitWall = true;
+                    }
+                }
+            }
+            int Ceiling = (float)(ScreenHeight / 2.0) - ScreenHeight / ((float)DistanceToWall);
+            int Floor = ScreenHeight - Ceiling;
+
+
+            for (int y = 0; y < ScreenHeight; y++) {
+                if (y < Ceiling) {
+                    screen[y * ScreenWidth + x] = ' ';
+                }
+                else if(y > Ceiling && y <= Floor) {
+                    screen[y * ScreenWidth + x] = '#';
+                }
+                else {
+                    screen[y * ScreenWidth + x] = ' ';
+                }
             }
 
         }
