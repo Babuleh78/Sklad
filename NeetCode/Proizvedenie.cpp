b56aa78ab0d8ca4326c963c8719abd0c8a835dc8
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-01-08 22:47:33 +0300

    Update Proizvedenie.cpp

--- NeetCode/Proizvedenie.cpp
+++ NeetCode/Proizvedenie.cpp
@@ -11,6 +11,7 @@ public:
             int number = nums[i+1]*l[0];
             l.insert(l.begin(), number);
         }
+        
         int size = r.size()-1;
         for(int i = 0; i <nums.size(); i++){
             ans.push_back(r[i]*l[i]);
