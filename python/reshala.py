530a5ba4288f3dce90a613af5fd0886bb7e7c43b
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-01-29 17:09:13 +0300

    Рисовалко

--- python/reshala.py
+++ python/reshala.py
@@ -1,5 +1,5 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # во избежание предупреждений
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -8,35 +8,74 @@ from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Dense, Flatten
 
-# Загружаем данные MNIST
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+import tkinter as tk
 
-# Нормализация изображений
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+class DrawingApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Рисовалко")
+        self.frame = tk.Frame(master)
+        self.frame.pack(padx=10, pady=10)
 
-# Измените форму входных данных с (60000, 28, 28) на (60000, 28, 28, 1)
-x_train = x_train.reshape(-1, 28, 28, 1)
-x_test = x_test.reshape(-1, 28, 28, 1)
+        self.canvas = tk.Canvas(self.frame, bg="white", width=150, height=150)
+        self.canvas.pack()
 
-# Преобразование меток в категориальный формат
-y_train_category = keras.utils.to_categorical(y_train, 10)
-y_test_category = keras.utils.to_categorical(y_test, 10)
+        self.last_x, self.last_y = None, None
 
-# Определение модели
-model = keras.Sequential([
-    Flatten(input_shape=(28, 28, 1)),
-    Dense(128, activation='relu'),
-    Dense(10, activation='softmax')
-])
+        self.canvas.bind("<Button-1>", self.start_draw)
+        self.canvas.bind("<B1-Motion>", self.draw)
+        self.canvas.bind("<ButtonRelease-1>", self.reset)
+        self.clear_button = tk.Button(master, text="Очистить", command=self.clear_canvas)
+        self.clear_button.pack(pady=5)
 
-# Компиляция модели
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    def start_draw(self, event):
+        self.last_x, self.last_y = event.x, event.y
 
-# Обучение модели
-model.fit(x_train, y_train_category, batch_size=32, epochs=5, validation_split=0.2)
+    def draw(self, event):
+        if self.last_x is not None and self.last_y is not None:
+            self.canvas.create_line(self.last_x, self.last_y, event.x, event.y, fill="black", width=2)
+    
+        self.last_x, self.last_y = event.x, event.y
 
-# Оценка модели
-test_loss, test_accuracy = model.evaluate(x_test, y_test_category)
-print(f"Тестовая точность: {test_accuracy}")
-print("Завершили")
+    def reset(self, event):
+        self.last_x, self.last_y = None, None
+
+    def clear_canvas(self):
+        self.canvas.delete("all")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DrawingApp(root)
+    root.mainloop()
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# x_train = x_train / 255.0
+# x_test = x_test / 255.0
+
+
+# y_train_category = keras.utils.to_categorical(y_train, 10)
+# y_test_category = keras.utils.to_categorical(y_test, 10)
+
+
+# model = keras.Sequential([
+#     Flatten(input_shape=(28, 28, 1)),
+#     Dense(100, activation = 'relu'),
+#     Dense(10, activation = 'softmax')
+# ])
+
+# model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# model.fit(x_train, y_train_category, batch_size = 32, epochs = 3, validation_split = 0.2)
+
+# model.evaluate(x_test, y_test_category)
+
+# n = 0
+
+# x = np.expand_dims(x_test[n], 0)
+# res = model.predict(x)
+# print(res)
+# print(np.argmax(res))
+
+# plt.imshow(x_test[n], cmap= plt.cm.binary)
+# plt.show()
+# print("Завершили")
