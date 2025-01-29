a2e9078fab041489076a7cf0ce4c329db796e504
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-01-29 16:28:02 +0300

    подбор ключа

--- python/reshala.py
+++ python/reshala.py
@@ -1,5 +1,5 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # во избежание предупреждений
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -8,32 +8,35 @@ from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Dense, Flatten
 
-
+# Загружаем данные MNIST
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train/255
-y_train = y_train/255
-#контроль векторов
+
+# Нормализация изображений
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Измените форму входных данных с (60000, 28, 28) на (60000, 28, 28, 1)
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+# Преобразование меток в категориальный формат
 y_train_category = keras.utils.to_categorical(y_train, 10)
 y_test_category = keras.utils.to_categorical(y_test, 10)
-# plt.figure(figsize=(10,5))
-# for i in range(25):
-#     plt.subplot(5, 5, i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.imshow(x_train[i], cmap = plt.cm.binary)
 
-# plt.show()
-
-
+# Определение модели
 model = keras.Sequential([
     Flatten(input_shape=(28, 28, 1)),
-    Dense(100, activation = 'relu'),
-    Dense(10, activation = 'softmax')
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+# Компиляция модели
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train_category, batch_size = 25, epochs = 3, validation_split = 0.2)
+# Обучение модели
+model.fit(x_train, y_train_category, batch_size=32, epochs=5, validation_split=0.2)
 
-model.evaluate(x_test, y_test_category)
+# Оценка модели
+test_loss, test_accuracy = model.evaluate(x_test, y_test_category)
+print(f"Тестовая точность: {test_accuracy}")
 print("Завершили")
