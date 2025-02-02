f6c0a218fd8fc53ac82a8fa009ba45f78525ac9e
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-02-02 14:45:09 +0300

    Update CrossWord.cpp

--- LeetCode/CrossWord.cpp
+++ LeetCode/CrossWord.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 using namespace std;
+
 int main() {
     int R, C;
     cin >> R >> C;
@@ -13,9 +14,9 @@ int main() {
         cin >> word;
         words.push_back(word);
     }
+
     string min_word = "";
 
-    // Проверка горизонтальных слов
     for (const auto& word : words) {
         if (word.length() >= 2 && word.find('#') == string::npos) {
             if (min_word.empty() || word < min_word) {
@@ -24,11 +25,18 @@ int main() {
         }
     }
 
-    // Проверка вертикальных слов
     for (int col = 0; col < C; ++col) {
         string vertical_word;
         for (int row = 0; row < R; ++row) {
-            vertical_word += words[row][col];
+            if (words[row][col] == '#') {
+                if (vertical_word.length() >= 2 && min_word.empty() || vertical_word < min_word) {
+                    min_word = vertical_word;
+                }
+                vertical_word.clear(); 
+            }
+            else {
+                vertical_word += words[row][col];
+            }
         }
         if (vertical_word.length() >= 2 && vertical_word.find('#') == string::npos) {
             if (min_word.empty() || vertical_word < min_word) {
@@ -37,9 +45,7 @@ int main() {
         }
     }
 
-    if (!min_word.empty()) {
-        cout << min_word << endl;
-    }
+    cout << min_word << endl;
 
     return 0;
 }
