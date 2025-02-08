cb16b67e99f329c433e5b35bb712d2b7705e6505
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-02-08 23:24:58 +0300

    Update Ostap.cpp

--- AgainAlgNothingInt/Ostap.cpp
+++ AgainAlgNothingInt/Ostap.cpp
@@ -3,26 +3,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main() 
+int main()
 {
 	int ans = 0;
 	int pr, ku;
-	cin >> pr; cin>> ku;
+	cin >> ku; cin >> pr;
 	vector<int> prod(pr);
 	vector<int> kupi(ku);
-	for(int i = 0; i<ku; i++){
-		cin>>kupi[i];
+	for (int i = 0; i < ku; i++) {
+		cin >> kupi[i];
 	}
 	sort(begin(kupi), end(kupi));
-	for(int i = 0; i<pr; i++){
-		cin>>prod[i];
+	for (int i = 0; i < pr; i++) {
+		cin >> prod[i];
 	}
 	sort(begin(prod), end(prod), greater<int>());
 	int i = 0;
-	while(i < pr && i < ku){
-		if(kupi[i] - prod[i] >0){
-			ans+=kupi[i] - prod[i];
-		} else{
+	while (i < pr && i < ku) {
+		if (-kupi[i] + prod[i] > 0) {
+			ans += -kupi[i] +prod[i];
+		}
+		else {
 			break;
 		}
 		i++;
