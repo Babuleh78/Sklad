80d26a608a520e765c3c8dba7b3f7cc3277b3846
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-02-19 17:19:37 +0300

    а ведь амням

--- python/photomaker/main.py
+++ python/photomaker/main.py
@@ -1,40 +1,37 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-
 root = tk.Tk()
 root.title("Трепещи перри утконос")
+root.geometry("1000x1000")
 
 image_path = 'main_photo.jpg' 
 image = Image.open(image_path)
 
-
 width, height = image.size
 
-WIDTH_CUT  = 4
-HEIGHT_CUT = 4
+WIDTH_CUT  = 32
+HEIGHT_CUT = 32
 small_width = width // WIDTH_CUT
 small_height = height // HEIGHT_CUT
 small_images = []
 
+# Список для хранения ссылок на изображения
+photo_images = []
 
-for i in range(WIDTH_CUT):  
-    for j in range(HEIGHT_CUT): 
+for i in range(HEIGHT_CUT): 
+    for j in range(WIDTH_CUT):  
         left = j * small_width
         upper = i * small_height
         right = left + small_width
         lower = upper + small_height
         
         small_image = image.crop((left, upper, right, lower))
-        small_images.append(small_image)
-
-
-tk_small_images = []
-
-for s_i in small_images:
-    tk_image_small = ImageTk.PhotoImage(s_i)  
-    tk_small_images.append(tk_image_small)  
-    label = tk.Label(root, image=tk_image_small)  
-    label.pack()
+        tk_image_small = ImageTk.PhotoImage(small_image) 
+        
+        photo_images.append(tk_image_small)
+        
+        label = tk.Label(root, image=tk_image_small)  
+        label.place(x=j * small_width, y=i * small_height) 
 
 root.mainloop()
