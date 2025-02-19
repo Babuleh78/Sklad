00169cd0339a450eb872eef8b1f7b27538c2f93a
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-02-19 20:58:57 +0300

    Update writedata.py

--- python/photomaker/writedata.py
+++ python/photomaker/writedata.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageTk
+from PIL import Image
+import json
 
 class Pixel:
     def __init__(self, r, g, b):
@@ -6,9 +7,10 @@ class Pixel:
         self.g = g
         self.b = b
 
-    def get_color(self):
-        return (self.r, self.g, self.b)
+    def to_dict(self):
+        return {'r': self.r, 'g': self.g, 'b': self.b}
 
+all_data = []
 image = Image.open("main_photo.jpg")
 
 width, height = image.size
@@ -17,6 +19,13 @@ WIDTH_CUT  = 4
 HEIGHT_CUT = 4
 small_width = width // WIDTH_CUT
 small_height = height // HEIGHT_CUT
+
+red_sum = 0
+green_sum = 0
+blue_sum = 0
+
+
+
 for i in range(HEIGHT_CUT): 
     for j in range(WIDTH_CUT):  
         left = j * small_width
@@ -36,7 +45,17 @@ for i in range(HEIGHT_CUT):
             red_sum += r
             green_sum += g
             blue_sum += b
-        print(red_sum, green_sum, blue_sum)
 
+        num_pixels = len(pixels)
+        average_red = red_sum // num_pixels
+        average_green = green_sum // num_pixels
+        average_blue = blue_sum // num_pixels
+
+        p = Pixel(average_red, average_green, average_blue)
+        all_data.append(p.to_dict())  
+
+
+with open('data.json', 'w') as f:
+    json.dump(all_data, f)
 
 print("Конец")
