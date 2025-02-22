a8f1aef9276f66918cc1be3bbb6d015e141816e7
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-02-22 21:51:27 +0300

    Update main.py

--- python/photomaker/main.py
+++ python/photomaker/main.py
@@ -1,72 +1,56 @@
-import tkinter as tk
+
 from PIL import Image, ImageTk
 from readdata import get_data
+import random
+import json
 
-import sys
+
 our_data = get_data()
 
 def find_closest(r, g, b):
     global our_data 
     global images
     min_diff = abs(our_data[0][0] - r)+abs(our_data[0][1] - g)+abs(our_data[0][2] - b)
+    closest_num = 0
     for i in range(len(our_data)):
         value = our_data[i]
         diff = abs(value[0] - r)+abs(value[1] - g)+abs(value[2] - b)
         if diff < min_diff:  
             min_diff = diff 
             closest_num = i
+    make_some_fun(closest_num)
     return closest_num
 
-def choose_one(a, b, c):#r ,g , b
+def make_some_fun(num):
     global our_data
-    ab = abs(a - b)
-    ac = abs(a - c)
-    bc = abs(b - c)
-
-    if ab <= ac and ab <= bc:
-    
-        return min(a, b)  
-    elif ac <= ab and ac <= bc:
-        
-        return min(a, c)  
-    else:
-        
-        return min(b, c)  
-    
+    for i in range(2):
+        rand = random.randint(0, 1)
+        if rand == 1:
+            our_data[num][i] += 5
+        else:
+            our_data[num][i] -= 5
 
 
-root = tk.Tk()
-root.title("Трепещи перри утконос")
-root.geometry("1100x1100")
+
 
 user_image_path = 'user_photo.jpg'
 user_image = Image.open(user_image_path) # то, что скинул пользователь
 user_image = user_image.resize((1024, 1024))
 width, height = user_image.size
 
-WIDTH_CUT  = 32
-HEIGHT_CUT = 32
+WIDTH_CUT  = 64
+HEIGHT_CUT = 64
 SMALL_WIDTH = width // WIDTH_CUT
 SMALL_HEIGHT = height // HEIGHT_CUT
 
 images = []
 
-for i in range(128):
+for i in range(170):
     image = Image.open(f"albums/{i+1}.jpg")
     image = image.resize((SMALL_WIDTH, SMALL_HEIGHT))
-    
-    tk_image = ImageTk.PhotoImage(image) 
 
-    images.append(tk_image)
+    images.append(image)
      
-  
-
-
-# if(WIDTH_CUT*HEIGHT_CUT!=len(our_data)):
-#     print("Ошибка с данным")
-#     sys.exit(1)
-# p = Pixel(our_data[0]['r'], our_data[0]['g'], our_data[0]['b'])
-# print(p)
 
 photo_images = []
 closest_numbers = []
@@ -78,8 +62,7 @@ for i in range(HEIGHT_CUT):
         lower = upper + SMALL_HEIGHT 
         
         small_image = user_image.crop((left, upper, right, lower))
-        tk_image_small = ImageTk.PhotoImage(small_image) 
-
+       
 
         pixels = list(small_image.getdata())
         red_sum = 0
@@ -99,11 +82,7 @@ for i in range(HEIGHT_CUT):
 
         closest_numbers.append(closest)
 
-for i in range(HEIGHT_CUT):
-    for j in range(WIDTH_CUT):
-        index = i * WIDTH_CUT + j  
-        if index < len(closest_numbers):  
-            tk_image_small = images[closest_numbers[index]]
-            label = tk.Label(root, image=tk_image_small, bd=0, highlightthickness=0)  
-            label.place(x=j * SMALL_WIDTH, y=i * SMALL_HEIGHT)
-root.mainloop()
+with open('images.json', 'w') as f:
+    json.dump(closest_numbers, f)
+
+
