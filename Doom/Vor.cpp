c271a0b06762b59e459a08e90e745e9518872323
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-02-24 20:35:14 +0300

    Update Vor.cpp

--- Doom/Vor.cpp
+++ Doom/Vor.cpp
@@ -8,60 +8,62 @@ using namespace std;
 
 void SetWindow(int Width, int Height)
 {
-	_COORD coord;
-	coord.X = Width;
-	coord.Y = Height;
-	_SMALL_RECT Rect;
-	Rect.Top = 0;
-	Rect.Left = 0;
-	Rect.Bottom = Height - 1;
-	Rect.Right = Width - 1;
-	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleScreenBufferSize(Handle, coord);
-	SetConsoleWindowInfo(Handle, TRUE, &Rect);
-	string str = "mode con cols=" + to_string(Width) + " lines=" + to_string(Height);
-	system(str.c_str());
+    _COORD coord;
+    coord.X = Width;
+    coord.Y = Height;
+    _SMALL_RECT Rect;
+    Rect.Top = 0;
+    Rect.Left = 0;
+    Rect.Bottom = Height - 1;
+    Rect.Right = Width - 1;
+    HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleScreenBufferSize(Handle, coord);
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);
+    string str = "mode con cols=" + to_string(Width) + " lines=" + to_string(Height);
+    system(str.c_str());
 }
 
 int main(void)
 {
-	int nwidth = 170;
-	float aspect = 11.0 / 24.0;
-	string path = "C:\\Users\\Денис\\Desktop\\Бауманка\\Sklad\\python\\photomaker\\del.jpg";
-	
-	Mat image = imread(path, IMREAD_GRAYSCALE);
-	imshow("", image);
-	Size sz = image.size();
-	int im_width = sz.width, im_height = sz.height;
-	float imasp = (float)im_width / nwidth;
-	int nheight = round(im_height * aspect / imasp);
+    int nwidth = 150;
+    float aspect = 11.0 / 24.0;
+    string path = "C:\\Users\\Денис\\Desktop\\Бауманка\\Sklad\\python\\photomaker\\del.jpg";
 
-	resize(image, image, { nwidth, nheight }, 0, 0, INTER_NEAREST);
-	SetWindow(nwidth, nheight);
+    Mat image = imread(path, IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        cerr << "Ошибка: не удалось загрузить изображение." << endl;
+        return -1;
+    }
 
-	wchar_t* screen = new wchar_t[nwidth * nheight + 1];
-	for (int i = 0; i < nwidth * nheight + 1; i++) screen[i] = ' ';
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
+    Size sz = image.size();
+    int im_width = sz.width, im_height = sz.height;
+    float imasp = (float)im_width / nwidth;
+    int nheight = round(im_height * aspect / imasp);
 
-	const char* gradient = " .:!/r(lZ4H9W8$@";
+    resize(image, image, { nwidth, nheight }, 0, 0, INTER_NEAREST);
+    SetWindow(nwidth, nheight);
 
-	for (int x = 0; x < image.rows; x++) {
-		for (int y = 0; y < image.cols; y++) {
-			try {
-				Vec3b pixel = image.at<Vec3b>(x, y);
-				int color = round((pixel[0] + pixel[1] + pixel[2]) / 3.0);
-				screen[x * nwidth + y] = gradient[color / 16];
-			}
-			catch (ERROR) {
-				print(Error)
-			}
+    wchar_t* screen = new wchar_t[nwidth * nheight + 1];
+    for (int i = 0; i < nwidth * nheight; i++) screen[i] = ' ';
+    screen[nwidth * nheight] = '\0'; // Завершающий нуль-символ
 
-		}
-	}
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
 
-	while (1) WriteConsoleOutputCharacter(hConsole, screen, nwidth * nheight, { 0, 0 }, &dwBytesWritten);
-	waitKey();
-	return 0;
+    const char* gradient = " .:!/r(lZ4H98W$@";
+
+    for (int x = 0; x < image.rows; x++) {
+        for (int y = 0; y < image.cols; y++) {
+            
+                uchar pixel = image.at<uchar>(x, y); 
+                int color = round(pixel);
+                screen[x * nwidth + y] = gradient[color / 16];
+           
+        }
+    }
+
+    while (1) WriteConsoleOutputCharacter(hConsole, screen, nwidth * nheight, { 0, 0 }, &dwBytesWritten);
+
+    return 0;
 }
