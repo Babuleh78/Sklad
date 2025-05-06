d3e90b9c025a62b740e03c1a4dde8f5a2819ccbd
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-05-06 14:08:44 +0300

    Update tinkall.cpp

--- AgainAlgNothingInt/tinkall.cpp
+++ AgainAlgNothingInt/tinkall.cpp
@@ -36,3 +36,61 @@ int main() {
 
     return 0;
 }
+
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+
+
+
+int main() {
+    int line_count;
+    cin >> line_count;
+    vector<pair<int, int>> metro_vector = vector<pair<int, int>>(line_count);
+
+    for (int i = 0; i < line_count; i++) {
+        pair<int, int> value;
+        cin >> value.first >> value.second;
+        metro_vector[i] = value;
+    }
+
+    int query_count;
+    cin >> query_count;
+    
+    int metro_line = 0;
+    int time = 0;
+    for (int i = 0; i < query_count; i++) {
+        cin >> metro_line >> time;
+
+        int first_train = metro_vector[metro_line - 1].first;
+        int interval = metro_vector[metro_line - 1].second;
+
+        int current_time;
+        if (time < first_train) {
+            current_time = first_train - time;
+        }
+        else {
+            int trains_passed = (time - first_train) / interval;
+            
+            int next_train_time = first_train + (trains_passed)*interval;
+            if (next_train_time == time) {
+                current_time = next_train_time;
+            }
+            else {
+                current_time = next_train_time + interval;
+            }
+            
+        }
+
+        cout << current_time << endl;
+
+    }
+
+    return 0;
+}
