89a3b42efe23877140a891cc179cca6eec986559
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-05-06 15:59:23 +0300

    Update tinkall.cpp

--- AgainAlgNothingInt/tinkall.cpp
+++ AgainAlgNothingInt/tinkall.cpp
@@ -93,4 +93,88 @@ int main() {
     }
 
     return 0;
+
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+struct Compare {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.first == b.first) {
+            return a.second > b.second;  
+        }
+        return a.first < b.first;  
+    }
+};
+
+
+long long maxTotalDifference(int n, vector<int>& a) {
+    if (n == 2) return abs(a[0] - a[1]);
+
+    vector<int> diffs(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+        diffs[i] = abs(a[i] - a[i + 1]);
+    }
+    priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> max_heap;
+
+    for (int i = 0; i < n - 1; ++i) {
+        max_heap.push({ diffs[i], i });
+    }
+
+    int size = a.size();
+    vector<bool> is_active(n - 1, true);
+    long long total = 0;
+
+    while (!max_heap.empty() && size>=2) {
+        auto current = max_heap.top();
+        max_heap.pop();
+        int diff = current.first;
+        int index = current.second;
+
+        if (!is_active[index]) continue;
+
+        total += diff;
+        is_active[index] = false;
+
+        int left = index;
+        while (left >= 0 && !is_active[left]) {
+            left--;
+        }
+
+        int right = index;
+        while (right < n - 1 && !is_active[right]) {
+            right++;
+        }
+
+        if (left >= 0 && right < n - 1) {
+            int new_diff = abs(a[left + 1] - a[right]);
+            diffs[left] = new_diff;
+            max_heap.push({ new_diff, left });
+            is_active[right] = false; 
+        }
+
+        size -= 2;
+    }
+
+    return total;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cout << maxTotalDifference(n, a) << endl;
+
+    return 0;
+}
 }
