850aa8ee95f84a9bcf16d986163823e03acce335
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-07-19 19:35:17 +0300

    Update kafka_tester_cons.py

--- Kafkamoment/kafka_tester_cons.py
+++ Kafkamoment/kafka_tester_cons.py
@@ -6,29 +6,75 @@ from aiokafka import AIOKafkaConsumer
 import json
 from datetime import datetime
 
+import sys
+from pathlib import Path
+
+import os
+import matplotlib.pyplot as plt
+import cv2
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import time
+
+sys.path.append(str(Path(__file__).parent.parent))
+from models.computer_vision_model.demo import cv_model
+from models.recognition_model.model import rec_model
+
+char_to_idx = {'A': 0, 'B': 1, 'E': 2, 'K': 3, 'M': 4, 'H': 5, # Словарь для преобразования символов в признаки
+                           'O': 6, 'P': 7, 'C': 8, 'T': 9, 'Y': 10, 'X': 11,
+                           '0': 12, '1': 13, '2': 14, '3': 15, '4': 16,
+                           '5': 17, '6': 18, '7': 19, '8': 20, '9': 21, '':22} 
+idx_to_char = {v: k for k, v in char_to_idx.items()} # Словарь, обратный предыдущему
+
 # Конфигурация
 BOOTSTRAP_SERVERS = "localhost:9092"
 TOPIC = "testtopic"
 
-# Моки моделей
-class MockCVModel:
-    async def predict(self, img):
-        return [type('obj', (object,), {
-            'boxes': type('obj', (object,), {
-                'xyxy': lambda: type('obj', (object,), {
-                    'cpu': lambda: type('obj', (object,), {
-                        'numpy': lambda: np.array([[10, 10, 100, 100]])
-                    })()
-                })()
-            })()
-        })]
+def preprocess_plate_image(plate_img):
+ 
+    plate_image = Image.fromarray(plate_img).convert('L')
+    
+    transform = transforms.Compose([
+        transforms.Resize((64, 320)),  
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5])  
+    ])
+   
+    image = transform(plate_image).unsqueeze(0)
+    
+    return image
 
-class MockRecModel:
-    async def recognize(self, img):
-        return "A123TEST"
+def decode_predictions(preds, idx_to_char): # Преобразование предсказаний модели в понятный результат
+    preds = preds.permute(1, 0, 2)  # [B, T, C] → [T, B, C]
+    probs = torch.softmax(preds, dim=2)
+    decoded = []
+    
+    for batch in probs:
+        chars = []
+        prev_char = None
+        for t in range(batch.shape[0]):
+            char_idx = torch.argmax(batch[t]).item()
+            if char_idx == 22:  # blank
+                prev_char = None
+                continue
+            if char_idx != prev_char:
+                chars.append(idx_to_char[char_idx])
+            prev_char = char_idx
+        decoded.append("".join(chars))
+    
+    return decoded
 
-cv_model = MockCVModel()
-rec_model = MockRecModel()
+def recognize_plate_text(plate_img, rec_model):
+    global idx_to_char
+    input_tensor = preprocess_plate_image(plate_img)
+    
+    with torch.no_grad():
+        outputs = rec_model(input_tensor)
+    
+    plate_text = decode_predictions(outputs, idx_to_char)[0]
+    
+    return plate_text
 
 async def recognize_plate_from_photo(photo_data: bytes, metadata: dict):
     try:
@@ -36,7 +82,7 @@ async def recognize_plate_from_photo(photo_data: bytes, metadata: dict):
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         
         detection_results = await cv_model.predict(img)
-        plate_text = await rec_model.recognize(img)
+        plate_text = await rec_model.recognize(detection_results)[0]
         
         print(f"Распознанный номер: {plate_text}")
         print(f"Метаданные: {metadata}")
