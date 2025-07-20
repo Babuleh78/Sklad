89530c460d1bd7507b4a6d4054f72c302ddb234e
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-07-20 19:30:10 +0300

    Update kafka_tester_prod.py

--- Kafkamoment/kafka_tester_prod.py
+++ Kafkamoment/kafka_tester_prod.py
@@ -1,15 +1,12 @@
 import asyncio
 from datetime import datetime
-import numpy as np
 import cv2
-from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from aiokafka import AIOKafkaProducer
 import json
-import sys
 from pathlib import Path
-import os
 
-import cv2
-import numpy as np
+from ultralytics import YOLO
+import time
 
 BOOTSTRAP_SERVERS = "localhost:9092"
 TOPIC = "testtopic"
@@ -39,20 +36,74 @@ class KafkaService:
 
 
 
-async def main():
+async def send_real_image():
     kafka_service = KafkaService()
-    
-    img = np.zeros((480, 640, 3), dtype=np.uint8)
-    cv2.putText(img, "TEST PLATE", (50, 50), 
-               cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    _, img_encoded = cv2.imencode('.jpg', img)
-    
-    metadata = {
-        "filename": "test_plate.jpg",
-        "source": "test_script"
-    }
-    await kafka_service.send_photo(img_encoded.tobytes(), metadata)
-  
+
+    try:
+        video_path = "test_video.mp4"
+        processing_interval = 0.5 # Сканируем каждые x секунды
+        
+        car_detection_model = YOLO('yolov8n.pt') # Consumer будет получать вырезанную машину (в реальности вряд ли пригодится)
+        
+        cap = cv2.VideoCapture(video_path)
+        if not cap.isOpened():
+            print("Ошибка открытия видеофайла")
+            return
+        
+        frame_count = 0
+        last_processing_time = time.time()
+        
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            frame_count += 1
+            current_time = time.time()
+
+            if current_time - last_processing_time < processing_interval:
+                continue
+                
+            last_processing_time = current_time
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+        
+            car_results = car_detection_model(frame_rgb, classes=[2, 5, 7], verbose=False)
+            
+            for car_result in car_results: # Передаем каждую машину
+                car_boxes = car_result.boxes.xyxy.cpu().numpy()
+                for car_box in car_boxes:
+                    x1, y1, x2, y2 = map(int, car_box[:4])
+                    
+                    car_img = frame[y1:y2, x1:x2]
+                    
+                    if car_img.size == 0:
+                        continue
+                    
+                    _, img_encoded = cv2.imencode('.jpg', car_img)
+                    
+                    try:
+                        await kafka_service.send_photo(img_encoded.tobytes(), metadata={
+                            'timestamp': current_time
+                        })
+                        print(f"Изображение автомобиля успешно отправлено")
+                    except Exception as e:
+                        print(f"Ошибка при отправке в Kafka: {e}")
+            
+            cv2.imshow('Shashura', frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+        
+    except Exception as e:
+        print(f"Ошибка при обработке видео: {e}")
+        raise
+
+async def main():
+    await send_real_image()
 
 
 if __name__ == "__main__":
