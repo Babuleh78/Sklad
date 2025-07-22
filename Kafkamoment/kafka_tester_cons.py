9662e541a0d703ff070ff00d5f0c1c313de77f81
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-07-22 15:58:54 +0300

    Update kafka_tester_cons.py

--- Kafkamoment/kafka_tester_cons.py
+++ Kafkamoment/kafka_tester_cons.py
@@ -7,6 +7,8 @@ import json
 import sys
 from pathlib import Path
 import time
+import redis
+import time
 
 sys.path.append(str(Path(__file__).parent.parent))
 from models.computer_vision_model.demo import cv_model
@@ -17,6 +19,22 @@ from models.demonstration import rotate_to_horizontal
 # Конфигурация
 BOOTSTRAP_SERVERS = "localhost:9092"
 TOPIC = "testtopic"
+
+r = redis.Redis(
+    host='localhost',
+    port=6379,
+    db=0,
+    decode_responses=True  # Для автоматического декодирования строк
+)
+
+def print_all_redis_data():
+    print("\nТекущее состояние парковки:")
+    for key in r.keys('*'):
+        entry_time = float(r.get(key))
+        parked_seconds = time.time() - entry_time
+        print(f"- {key}: на парковке {parked_seconds:.2f} сек")
+
+
 async def recognize_plate_from_photo(photo_data: bytes, metadata: dict):
 
     global plate_confirmation, last_detection_time
@@ -27,20 +45,22 @@ async def recognize_plate_from_photo(photo_data: bytes, metadata: dict):
     
     CONFIRMATION_COUNT = 3  # Необходимое количество подтверждений
     CONFIRMATION_TIMEOUT = 3.0  # Максимальное время между подтверждениями (сек)
+    REDIS_MAX_ITEMS = 5 # Максимальное количество предметов в кэше
+    REDIS_TTL = 120  # Максимальное время хранения предмета в кэше
     
-    try: # Везде, где возвращается NONE в последствии убрать print, чтобы не засорять логи
+    try: 
         current_time = time.time()
         nparr = np.frombuffer(photo_data, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         if img is None:
-            print("Не удалось декодировать изображение")
+            #print("Не удалось декодировать изображение")
             return None
         
        
         detection_results = cv_model.predict(img, verbose=False)
         if not detection_results or len(detection_results[0].boxes) == 0:
-            print("Номера не обнаружены")
+            #print("Номера не обнаружены")
             return None
 
         license_plates = []
@@ -53,25 +73,25 @@ async def recognize_plate_from_photo(photo_data: bytes, metadata: dict):
                     license_plates.append(license_plate)
         
         if not license_plates:
-            print("Не удалось извлечь области с номерами")
+            #print("Не удалось извлечь области с номерами")
             return None
         
         corrected_plate = rotate_to_horizontal(license_plates[0])
         results = cv_model.predict(corrected_plate, verbose=False)
         if not results or len(results[0].boxes) == 0:
-            print("Не удалось уточнить позицию номера после поворота")
+            #print("Не удалось уточнить позицию номера после поворота")
             return None
     
         x1, y1, x2, y2 = results[0].boxes.xyxy[0].cpu().numpy().astype(int)
         final_plate = corrected_plate[y1:y2, x1:x2]
 
         if final_plate.size == 0:
-            print("Пустая область после уточнения")
+            #print("Пустая область после уточнения")
             return None
                     
         plate_text = recognize_plate_text(final_plate, rec_model)
         if not plate_text.strip():
-            print("Не удалось распознать текст номера")
+            #print("Не удалось распознать текст номера")
             return None
         
         clean_plate_text = ''.join(c for c in plate_text if c.isalnum()).upper()
@@ -91,14 +111,33 @@ async def recognize_plate_from_photo(photo_data: bytes, metadata: dict):
         
         if current_state['count'] >= CONFIRMATION_COUNT and not current_state['confirmed']:
             current_state['confirmed'] = True
-            print(f"Подтвержденный номер: {clean_plate_text} (подтверждений: {current_state['count']})")
-            print(f"Метаданные: {metadata}")
-           
-            
             plate_confirmation[clean_plate_text] = current_state
-            return clean_plate_text
+            # Пошел redis
+            # Номер есть - машина въезжает
+            if r.exists(clean_plate_text):
+                entry_time = float(r.get(clean_plate_text)) # Сейчас в Редисе пара Номер - время
+                parking_time = time.time() - entry_time
+                print(f"Машина с номером {clean_plate_text} простояла {parking_time:.2f} секунд и выехала")
+                print_all_redis_data()
+                
+                r.delete(clean_plate_text) # Машина уехала
+                return clean_plate_text
+            # Если номера нет - это въезд
+            else:
+                # Если Redis переполнен, удаляем самый старый номер
+                if r.dbsize() >= REDIS_MAX_ITEMS:
+                    oldest_key = min(r.keys('*'), key=lambda k: float(r.get(k)))
+                    r.delete(oldest_key)
+                    print(f"Удален самый старый номер: {oldest_key}")
+                
+                # Добавляем номер с текущей временной меткой
+                r.setex(clean_plate_text, REDIS_TTL, time.time())
+                print(f"Машина с номером {clean_plate_text} заехала на парковку")
+                print_all_redis_data()
+                
+                return clean_plate_text
         else:
-            print(f"Предварительное распознавание: {clean_plate_text} (подтверждений: {current_state['count']}/{CONFIRMATION_COUNT})")
+            #print(f"Предварительное распознавание: {clean_plate_text} (подтверждений: {current_state['count']}/{CONFIRMATION_COUNT})")
             plate_confirmation[clean_plate_text] = current_state
             return None
             
