6c9aea5ba1635d87c9adc207027f93e9d10ee47b
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-07-25 16:18:20 +0300

    Update kafka_tester_prod.py

--- Kafkamoment/kafka_tester_prod.py
+++ Kafkamoment/kafka_tester_prod.py
@@ -1,27 +1,16 @@
 import asyncio
-from datetime import datetime
 import cv2
 from aiokafka import AIOKafkaProducer
 import json
 from ultralytics import YOLO
 import time
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from matplotlib.gridspec import GridSpec
-import ipywidgets as widgets
-from IPython.display import display
-import asyncio
-import cv2
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-import time
-from mpl_interactions import panhandler, zoom_factory
+import subprocess
+import numpy as np
+import threading
 
 BOOTSTRAP_SERVERS = "localhost:9092"
 TOPIC = "testtopic"
-
-
 
 class KafkaService:
     def __init__(self):
@@ -37,12 +26,11 @@ class KafkaService:
             message = {
                 "photo": photo_data.hex(),
                 "metadata": metadata,
-                "timestamp": datetime.now().isoformat()
+                "timestamp": time.time()
             }
             await producer.send(TOPIC, message)
         finally:
             await producer.stop()
-
 
 
 async def video_test():
@@ -95,7 +83,8 @@ async def video_test():
                         await kafka_service.send_photo(img_encoded.tobytes(), metadata={
                             'timestamp': current_time
                         })
-                        #print(f"Изображение автомобиля успешно отправлено")
+
+                
                     except Exception as e:
                         print(f"Ошибка при отправке в Kafka: {e}")
             
@@ -166,9 +155,133 @@ async def image_test():
         print(f"Ошибка в image_test: {e}")
         raise
 
+async def stream_test():
+    kafka_service = KafkaService()
+    twitch_url = "https://www.twitch.tv/mobotixwebcamsrussia"
+    
+    command = [
+        'streamlink', # Утилита для получения данных
+        twitch_url, # Ссылка
+        'best', # Выбираем лучшее качество
+        '--stream-url', # Требуем вернуть stream-url
+        '--retry-streams', '30', # Попытки подключеня каждые 30 сек
+        '--retry-max', '5', # Максимальное число попыток
+    ]
+    
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate(timeout=10)
+        if process.returncode != 0:
+            raise Exception(f"Streamlink error: {stderr.decode()}")
+        
+        stream_url = stdout.decode().strip()
+        
+        ffmpeg_command = [
+            'ffmpeg',
+            '-i', stream_url,           # Входной поток
+            '-fflags', '+genpts+discardcorrupt',  # Исправление временных меток
+            '-flags', 'low_delay',      # Минимизация задержки
+            '-avioflags', 'direct',     # Прямой доступ к данным
+            '-strict', 'experimental',  # Разрешение экспериментальных кодеков
+            '-f', 'image2pipe',         # Формат вывода — поток изображений
+            '-pix_fmt', 'bgr24',        # Цветовой формат 
+            '-vcodec', 'rawvideo',      # Кодек для вывода (сырое видео)
+            '-vsync', '0',              # Отключение синхронизации по FPS
+            '-bufsize', '64K',          # Уменьшение буфера (для снижения задержки)
+            '-probesize', '32',         # Минимальный анализ входного потока
+            '-analyzeduration', '0',    # Отключение анализа длительности
+            '-loglevel', 'error',       # Вывод только ошибок
+            '-'                         # Вывод в stdout 
+        ]
+        
+        # Запускаем ffmpeg с отдельным stderr
+        process = subprocess.Popen(
+            ffmpeg_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            bufsize=10**8
+        )
+        
+        # Читаем stderr ffmpeg в отдельном потоке
+        def log_stderr():
+            while True:
+                line = process.stderr.readline()
+                if not line:
+                    break
+                print("FFMPEG ERROR:", line.decode().strip())
+        
+        
+        stderr_thread = threading.Thread(target=log_stderr, daemon=True)
+        stderr_thread.start()
+        
+        width, height = 1280, 720  
+        
+        car_detection_model = YOLO('yolov8n.pt')
+        processing_interval = 0.25
+        last_processing_time = time.time()
+        
+        try:
+            frame_size = width * height * 3 # 3- количество байт на пиксель
+            while True:
+                # Читаем кадр
+                raw_frame = process.stdout.read(frame_size)
+                frame = np.frombuffer(raw_frame, dtype='uint8').reshape((height, width, 3))
+                
+                current_time = time.time()
+                if current_time - last_processing_time < processing_interval:
+                    continue
+                
+                last_processing_time = current_time
+                
+                # Детекция машин
+                car_results = car_detection_model(frame, classes=[2, 5, 7], verbose=False)
+                boxes = car_results[0].boxes.xyxy.cpu().numpy()  
+                confidences = car_results[0].boxes.conf.cpu().numpy()  
+                num_cars = min(3, len(boxes))  
+
+                for i in range(num_cars):
+                    car_box = boxes[i]
+                    confidence = confidences[i]
+                    
+                    x1, y1, x2, y2 = map(int, car_box[:4])
+                    car_img = frame[y1:y2, x1:x2]
+                    
+                    if car_img.size == 0:
+                        continue
+                    
+                    _, img_encoded = cv2.imencode('.jpg', car_img)
+                    
+                    try:
+                        await kafka_service.send_photo(
+                            img_encoded.tobytes(),
+                            metadata={
+                                'timestamp': current_time,
+                                'confidence': float(confidence)
+                            }
+                        )
+                    except Exception as e:
+                        print(f"Ошибка Kafka: {e}")
+
+                
+                cv2.imshow('Twitch Stream', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        
+        finally:
+            process.terminate()
+            try:
+                cv2.destroyAllWindows()
+            except:
+                pass
+    
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        raise
+
 async def main():
     #await video_test()
-    await image_test()
+    #await image_test()
+    await stream_test()
 
 
 if __name__ == "__main__":
