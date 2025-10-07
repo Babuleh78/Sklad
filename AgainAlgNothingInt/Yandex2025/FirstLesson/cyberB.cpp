d7b8db558891d3129a3a40a3c3175f45e8d48893
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2025-10-07 21:59:59 +0300

    Update cyberB.cpp

--- AgainAlgNothingInt/Yandex2025/FirstLesson/cyberB.cpp
+++ AgainAlgNothingInt/Yandex2025/FirstLesson/cyberB.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <unordered_map>    
-#include <string>
-int main() {
-    std::string s;
-    std::cin >> s;
+    #include <iostream>
+    #include <iomanip>
+    #include <algorithm>
+    #include <unordered_map>    
+    #include <string>
+    int main() {
+        std::string s;
+        std::cin >> s;
 
 
-    std::unordered_map<char, int> hash;
+        std::unordered_map<char, long long> hash;
 
-    for (char symb : s){
-        hash[symb]++;
+        for (char symb : s){
+            hash[symb]++;
+        }
+
+        long long n = s.size();
+        long long ans = 0;
+
+
+        for (auto& it : hash){
+            ans += it.second * (-it.second + n);
+        }
+
+        std::cout << ans/2 +1;
+        return 0;
     }
-
-    int n = s.size();
-    int ans = 0;
-
-
-    for (auto& it : hash){
-        ans += it.second * (-it.second + n);
-    }
-
-    std::cout << ans/2 +1;
-    return 0;
-}
